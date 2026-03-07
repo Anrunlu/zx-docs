14c34d171387f2088a9e07866b803a63e468b047
zilizhou <zlzhou999@163.com>
2026-03-07 11:21:23 +0800

    修改了sidebar中的内容

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -2,12 +2,12 @@ import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
   "/teachbook/python/": "structure",
-  // "/teachbook/harmony/": "structure",
-  // "/teachbook/algorithm/": "structure",
-  // "/teachbook/tecinnovation/": "structure",
-  // "/teachbook/chineseMedical/": "structure"
-  // "/teachbook/machineLearning/": "structure"
-    "/teachbook/kgandnlp/": "structure",
+  "/teachbook/harmony/": "structure",
+  "/teachbook/algorithm/": "structure",
+  "/teachbook/tecinnovation/": "structure",
+  "/teachbook/chineseMedical/": "structure",
+  "/teachbook/machineLearning/": "structure"
+  "/teachbook/kgandnlp/": "structure",
   "/teachbook/pythonAI/": "structure"
 
 
